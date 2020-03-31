a237e5d71f151ad2f427ff25871ca637262a2822
syouligan <syoulten@gmail.com>
2020-03-31 12:16:33 -0600

    Full pipeline

--- SC_submit_scripts.sh
+++ SC_submit_scripts.sh
@@ -12,7 +12,7 @@ scriptsPath='/home/scoyou/sarah_projects/'
 logDir=$scriptsPath$projectID'/'$projectID'_scripts/logs'
 mkdir -p $logDir
 
-#qsub -P OsteoporosisandTranslationalResearch -N 'Determine_thresholds_'$projectID -b y -wd $logDir -j y -R y -l mem_requested=8G -pe smp 32 -V -m bea -M s.youlten@garvan.org.au R CMD BATCH $scriptsPath$projectID'/'$projectID'_scripts/Determine_prefiltering_thresholds.R'
+qsub -P OsteoporosisandTranslationalResearch -N 'Determine_thresholds_'$projectID -b y -wd $logDir -j y -R y -l mem_requested=8G -pe smp 32 -V -m bea -M s.youlten@garvan.org.au R CMD BATCH $scriptsPath$projectID'/'$projectID'_scripts/Determine_prefiltering_thresholds.R'
 qsub -P OsteoporosisandTranslationalResearch -N 'Prefiltering_'$projectID -b y -hold_jid 'Determine_thresholds_'$projectID -wd $logDir -j y -R y -l mem_requested=8G -pe smp 32 -V -m bea -M s.youlten@garvan.org.au R CMD BATCH $scriptsPath$projectID'/'$projectID'_scripts/Prefiltering_whole_experiment.R'
 qsub -P OsteoporosisandTranslationalResearch -N 'PseudoDGE_tissue_'$projectID -b y -hold_jid 'Prefiltering_'$projectID -wd $logDir -j y -R y -l mem_requested=8G -pe smp 32 -V -m bea -M s.youlten@garvan.org.au R CMD BATCH $scriptsPath$projectID'/'$projectID'_scripts/Pseudo-bulk_DGE_by_tissue.R'
 qsub -P OsteoporosisandTranslationalResearch -N 'NormyCluster_'$projectID -b y -hold_jid 'Prefiltering_'$projectID -wd $logDir -j y -R y -l mem_requested=8G -pe smp 32 -V -m bea -M s.youlten@garvan.org.au R CMD BATCH $scriptsPath$projectID'/'$projectID'_scripts/Whole_experiment_batch_correction_and_clustering.R'
